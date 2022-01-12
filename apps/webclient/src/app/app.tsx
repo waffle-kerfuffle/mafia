9050f4d182e6010984a7f24dbe615248ef1018c4
shockylemi <shockylemi@gmail.com>
2022-01-12 10:48:10 +0330

    initial commit

---
+++ apps/webclient/src/app/app.tsx
@@ -0,0 +1,13 @@
+import './app.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="webclient" />
+      <div />
+    </>
+  );
+}
+
+export default App;
